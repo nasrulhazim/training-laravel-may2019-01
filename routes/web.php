91cb2c05a9d53875d2205f406ce82a15f3a5b4c4
Nasrul Hazim <nasrulhazim.m@gmail.com>
2019-05-11 14:29:50 +0800

    Auth Scaffolding

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Auth::routes();
+Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
 // Route::resource('users', 'UserController');
 
